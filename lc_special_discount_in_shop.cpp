36b83d2ce88f6f6f32828a6461eb23a19f53ca4b
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-10-29 07:06:58 +0530

    Special discount in shop
    https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/

---
+++ lc_special_discount_in_shop.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n=prices.size();
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(prices[j]<=prices[i])
+                {
+                    prices[i]=prices[i]-prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+};
