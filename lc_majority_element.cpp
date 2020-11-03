1e8214927a8a2386ae5943ad9e1364f0d21c7cbb
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-11-03 14:14:48 +0530

    Majority element in array
    Using Moore Algorithm 
    https://leetcode.com/problems/majority-element/

---
+++ lc_majority_element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int element=0;
+        for(int num:nums)
+        {
+            if(count==0)
+                element=num;
+            if(num==element)
+                count++;
+            else
+                count--;
+        }
+        return element;
+    }
+};
