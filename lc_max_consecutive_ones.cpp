6a19eb01189a26b6fe7d64fcebe0b28930fd237f
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-11-04 09:06:31 +0530

    Max consecutive ones
    https://leetcode.com/problems/max-consecutive-ones/

---
+++ lc_max_consecutive_ones.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0,ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1)
+            {
+                count++;
+            }
+            else
+            {
+                ans =max(ans,count);
+                count=0;
+            }
+            ans =max(ans,count);
+        }
+        return ans;
+    }
+};
