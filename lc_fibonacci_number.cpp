e1c7df4975241b42b146c6138f95c26d2d6c6da9
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-11-04 08:43:28 +0530

    Fibonacci number
    https://leetcode.com/problems/fibonacci-number/

---
+++ lc_fibonacci_number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int fib(int N) {
+        int t1=1,t2=1,nextTerm=0;
+        if(N==0){
+            return 0;
+        }
+        else if(N==1){
+            return 1;
+        }
+        else if(N==2){
+            return 1;
+        }
+        else
+        {
+            for(int i=3;i<=N;i++)
+            {
+                nextTerm=t1+t2;
+                t1=t2;
+                t2=nextTerm;
+            }
+            return nextTerm;
+        }
+
+    }
+};
