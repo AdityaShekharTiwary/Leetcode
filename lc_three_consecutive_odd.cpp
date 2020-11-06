3dbafcc3cd4657a831ae51d4787bcf6d2e5fbd97
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-11-06 11:51:07 +0530

    1550. Three Consecutive Odds
    https://leetcode.com/problems/three-consecutive-odds/

---
+++ lc_three_consecutive_odd.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n=arr.size();
+        int count=0,flag=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]%2==1)
+                count++;
+            else
+                count=0;
+            if(count==3)
+                return true;
+        }
+        return false;
+
+
+
+    }
+};
