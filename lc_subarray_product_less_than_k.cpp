01e841c436b0f6bd42c561f3aa314a9670a0b679
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-11-27 08:57:40 +0530

    713. Subarray Product Less Than K
    https://leetcode.com/problems/subarray-product-less-than-k/

---
+++ lc_subarray_product_less_than_k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(k<=1)
+            return 0;
+
+        int left=0;
+        int right=0;
+
+        int prod=1;
+        int result=0;
+
+        while(right<n){
+            prod*=nums[right];
+
+            while(prod>=k){
+                prod/=nums[left];
+                left++;
+            }
+            result += right-left+1;
+            right++;
+        }
+        return result;
+    }
+};
