d7b017d6c76f0e6b883e4ceac0fde1e19774a46a
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-01-18 19:41:29 +0530

    Right view of binary Tree

---
+++ lc_right_view_of_binary_tree.cpp
@@ -0,0 +1,19 @@
+vector<int> rightView(Node *root)
+{
+   vector<int>res;
+   if(root==NULL) return res;
+   queue<Node*> q;
+   q.push(root);
+   while(!q.empty()){
+       int n=q.size();
+       for(int i=0;i<n;i++){
+           Node* temp=q.front();
+           q.pop();
+           if(i==0) res.push_back(temp->data);
+           if(temp->right) q.push(temp->right);
+           if(temp->left) q.push(temp->left);
+       }
+   }
+   return res;
+}
+
