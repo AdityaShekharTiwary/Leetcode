8f1867868283f5a0b3ec8a6653c23781626899b3
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-01-30 12:35:08 +0530

    96. Unique Binary Search Trees
    https://leetcode.com/problems/unique-binary-search-trees/

---
+++ lc_unique_binary_search_tree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> ans(n+1);
+        ans[0] = 1;
+        for(int i=1; i<=n; ++i){
+           for(int j=0; j<i; ++j)
+              ans[i]+=(ans[j]*ans[i-1-j]);
+        }
+        return ans[n];
+    }
+};
