b4f8da9067e27504b1f3aeb69fbdf6f9c17a8335
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-04-12 23:33:36 +0530

    1672. Richest Customer Wealth

---
+++ lc_richest_customer_wealth.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int res=0;
+        for(int i=0;i<accounts.size();i++){
+            int sum=0;
+            for(int j=0;j<accounts[i].size();j++){
+                sum+=accounts[i][j];
+                res=max(sum,res);
+            }
+        }
+        return res;
+    }
+};
