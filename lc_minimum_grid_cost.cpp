32d50bbb81fd5e8a7043f7e33bee352dfffa10bb
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-05-01 21:30:37 +0530

    Minimum grid cost

---
+++ lc_minimum_grid_cost.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp (n, vector<int> (m));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (i == n - 1 && j == m - 1) dp[i][j] = grid[i][j];
+                else if (i == n - 1) dp[i][j] = dp[i][j + 1] + grid[i][j];
+                else if (j == m - 1) dp[i][j] = dp[i + 1][j] + grid[i][j];
+                else dp[i][j] = min(dp[i + 1][j], dp[i][j + 1]) + grid[i][j];
+            }
+        }
+        return dp[0][0];
+    }
+};
