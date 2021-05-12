267aa45af3cd06cfbe60dadc326766be914f3d57
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-05-12 16:25:18 +0530

    384. Shuffle an Array

--- lc_suffle_the_array.cpp
+++ lc_suffle_the_array.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
-    vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans;
-        for(int i=0,j=n;i<n;i++,j++)
-        {
-            ans.push_back(nums[i]);
-            ans.push_back(nums[j]);
+    vector<int> nums;
+public:
+    Solution(vector<int> nums) {
+        this->nums = nums;
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return nums;
+    }
+
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        vector<int> result(nums);
+        for (int i = 0; i < result.size(); i++) {
+            int pos = rand() % (result.size() - i);
+            swap(result[i + pos], result[i]);
         }
-        return ans;
+        return result;
     }
 };
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
