b14b309eeacc36bc27df309c4161f8bb4f408ee9
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-05-19 16:38:53 +0530

    134. Gas Station

---
+++ lc_gas_station.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int total = 0;
+        for (int i = 0; i < gas.size(); i++) total += gas[i] - cost[i];
+        if (total < 0) return -1;
+        int start = 0;
+        int tank = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return start;
+    }
+};
