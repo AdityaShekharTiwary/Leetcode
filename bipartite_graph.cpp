cde81f7a5dd9e931de7e95b8219f3d4d2677c872
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-08-28 10:14:12 +0530

    785. Is Graph Bipartite?

---
+++ bipartite_graph.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+    bool bipartiteBFS(vector<vector<int>>& graph, vector<int> &color, int idx) {
+        queue<int> q;
+        q.push(idx);
+        color[idx] = 1;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            for (auto it : graph[node]) {
+                if (color[it] == -1) {
+                    color[it] = 1 - color[node];
+                    q.push(it);
+                }
+                else {
+                    if (color[it] == color[node]) return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool bipartiteDFS(vector<vector<int>>& graph, vector<int> &color, int node) {
+        if (color[node] == -1) color[node] = 1;
+        for (auto it : graph[node]) {
+            if (color[it] == -1) {
+                color[it] = 1 - color[node];
+                if (!bipartiteDFS(graph, color, it)) return false;
+            }
+            else if (color[it] == color[node]) return false;
+        }
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1);
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                // if(!bipartiteBFS(graph,color,i)) return false;
+                if (!bipartiteDFS(graph, color, i)) return false;
+            }
+        }
+        return true;
+    }
+};
